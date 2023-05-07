52c72c169b368ba097e9900f37b67af287e6a205
lukacode <lucassabe2002@gmail.com>
2023-05-07 20:28:37 -0300

    function study

---
+++ src/IdeArchivosJavaySintaxis/AdiosMundo.java
@@ -0,0 +1,13 @@
+package IdeArchivosJavaySintaxis;
+
+public class AdiosMundo {
+
+    public static void main(String[] args) {
+
+        //crear un public static void main nos permite tener un botos play
+        //para poder ejecutar la funcion que creemos
+
+        System.out.println("oh yeah");
+    }
+
+}
