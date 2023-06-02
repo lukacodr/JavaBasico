0bd453bd31504e2a79a40547d690d95422efcef9
lukacodr <lucassabe2002@gmail.com>
2023-06-02 17:33:34 -0300

    progress

---
+++ src/EventosXMLyJSON/EjemploJSON/Fundamentos.java
@@ -0,0 +1,3 @@
+//Cuando tendremos mas campos utilizaremos coma ","
+//El ultimo elemento es el unico que no va con coma
+//Un tokenizador va leyendo caracter a caracter. Segun lo que encuentre te dice lo que es y lo que no es
